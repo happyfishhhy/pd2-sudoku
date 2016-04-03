ac6f62c5594bc8443d725b891492a7bf41dae3af
Happyfishhhy <F74046161@2016cpp.(none)>
2016-04-04 02:01:24 +0800

    add transform.cpp

---
+++ transform.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <algorithm>
+#include "Sudoku.h"
+using namespace std;
+
+int main()
+{
+	Sudoku ss;
+	ss.readIn();
+	ss.transform();
+
+	return 0;
+}
